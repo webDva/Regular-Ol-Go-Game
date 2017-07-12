f9490934edce91432f992b52dcd98f2cfd78f6e4
Web D.va <webNoDva@gmail.com>
2017-07-12 19:57:36 -0400

    Not going to use a BST.

--- src/logic/board.js
+++ src/logic/board.js
@@ -2,13 +2,25 @@
  * Provides a representation of the board.
  */
 
-class Board {
+// enum-like constants
+export const EMPTY_POINT = 0;
+export const PLAYER_ONE_FILLED_POINT = 1;
+export const PLAYER_TWO_FILLED_POINT = 2;
+
+class Board {    
     /** For now, instances will do.
      * @param {Number} dimensions The length of the sides.
      */
     constructor(dimensions = 9) {
         this.dimensions = dimensions; // default dimensions is 9x9
-        // Use an array to contain the binary search tree that represents the grid.
-        this.grid = new Array(dimensions * dimensions);
+        this.gridPoints = []; // 2D array to contain grid points
+                
+        // Create a 2D array to represent the board.
+        for (let i = 0; i < dimensions; i++) {
+            this.gridPoints.push([]);
+            for (let j = 0; j < dimensions; j++) {
+                this.gridPoints[i].push(EMPTY_POINT);
+            }
+        }
     }
 }
